ba43cfc34e3152cbd964ab13da603d625e86e263
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-04 17:28:41 +0300

    user-defined methods

--- src/com/pentacom/Main.java
+++ src/com/pentacom/Main.java
@@ -130,10 +130,34 @@ public class Main {
 
         //methods
         ///built-in methods e.g. println() is a method
-        ///user predefined methods
+        ///user pre-defined methods
 
-        String nome = "Giraff";
-        boolean testNome = nome.startsWith("G") && nome.endsWith("e");
-        System.out.println(testNome);
-    }}
+//        String nome = "Giraff";
+//        boolean testNome = nome.startsWith("G") && nome.endsWith("e");
+//        System.out.println(testNome);
+
+        ///user pre-defined methods
+    char [] letters = {'A', 'A', 'B', 'C', 'D', 'D', 'D' };
+
+    //calling/invoking the method defined below
+    int count = countOccurrences(letters, 'D');
+        System.out.println(count);
+    }
+
+    //"public" is an access-modifier which makes the method accessible by other classes
+    //"static" means the method belongs to a class
+    //"int" is the return type of this method
+    public static int countOccurrences(char [] letters, char searchLetter){
+        System.out.println(Arrays.toString(letters));
+        System.out.println(searchLetter);
+        int count = 0;
+        for (char letter : letters){
+            if (letter == searchLetter){
+                count++;
+            }
+        }
+       return count;
+    }
+
+}
 
