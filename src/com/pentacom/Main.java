5df78e48fefef025ee289e0e0310a78a728be299
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-04 10:31:05 +0300

    advanced way for loop through array

--- src/com/pentacom/Main.java
+++ src/com/pentacom/Main.java
@@ -45,9 +45,20 @@ public class Main {
 //        System.out.println(numbers[3]);
 
         //Normal for loop
-        for (int i = 0; i < numbers.length; i++){
-            System.out.println(numbers[i]);
-        }
+//        for (int i = 0; i < numbers.length; i++){
+//            System.out.println(numbers[i]);
+//        }
+
+
+        //intellij shortcut--> type " numbers.fori" then hit TAB
+//        for (int i = 0; i < numbers.length; i++) {
+//
+//        }
+
+        //Reversed loop  //intellij shortcut--> type " numbers.forr" then hit TAB
+//        for (int i = numbers.length - 1; i >= 0; i--) {
+//
+//        }
 
         //Enhanced for loop
 //        for (int number : numbers){
@@ -55,9 +66,12 @@ public class Main {
 //        }
 
 
-
 //        String [] names = {"ALi", "Maria", "Dave"};
 //        System.out.println(Arrays.toString(names));
 //        System.out.println(names.length);
+
+        //Advanced way to loop through an array
+        Arrays.stream(numbers).forEach(System.out::println);
+
     }}
 
