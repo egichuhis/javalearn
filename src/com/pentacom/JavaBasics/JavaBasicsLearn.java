a091614c22f442872aa5a7054b3a6469a6088399
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-05 14:02:07 +0300

    Scanner Class refactor

--- src/com/pentacom/JavaBasics/JavaBasicsLearn.java
+++ src/com/pentacom/JavaBasics/JavaBasicsLearn.java
@@ -6,30 +6,6 @@ public class JavaBasicsLearn {
 
     public static void main(String[] args) {
 
-
-
-
-
-        //Scanner class --> take user input from the console
-
-//        Scanner myScanner = new Scanner(System.in);
-//        System.out.println("Enter your username: ");
-//        String userName = myScanner.nextLine();
-//        System.out.println("Hello " + userName);
-//
-//        System.out.println("How old are you? ");
-//        int age = myScanner.nextInt();
-//        int year = LocalDate.now().minusYears(age).getYear();
-//        System.out.println("You were born in " + year);
-//
-//        if (age > 18){
-//            System.out.println("You are an adult :)");
-//        }
-//        else{
-//            System.out.println("You are not an adult");
-//        }
-//
-
         //methods
         ///built-in methods e.g. println() is a method
         ///user pre-defined methods
