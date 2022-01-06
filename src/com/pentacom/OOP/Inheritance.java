13bc724416bfe84185bd2bcfdc77ecf1454c7b93
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-06 16:30:23 +0300

    object definition in the class

---
+++ src/com/pentacom/OOP/Inheritance.java
@@ -0,0 +1,39 @@
+package com.pentacom.OOP;
+
+//If you want anything which should belong to class make it static!!
+class Product {
+
+    //Attributes (State)
+    int pid;
+    String name;
+    int price;
+
+    //Constructor
+    Product(){
+        System.out.println(">> Product Object Constructed");
+        }
+
+        //Methods (Behavior)
+    //To write data in Product Object we have this Method
+    void setProductDetails(int pid, String name, int price){
+        this.pid = pid;
+        this.name = name;
+        this.price = price;
+    }
+
+    //To read data from Product Object
+    void showProductDetails(){
+        System.out.println("Product ID" + pid);
+        System.out.println("Product Name:\t" + name);
+        System.out.println("Product Price:\t" + price);
+        System.out.println("------------------------------------------------------");
+    }
+}
+
+
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/pentacom/OOP/Notes.java
@@ -0,0 +1,53 @@
+package com.pentacom.OOP;
+
+public class Notes {
+
+    /*
+    Object-Oriented Programming
+    OOP Concepts
+    Inheritance, Polymorphism, Abstraction, Encapsulation
+
+    Inheritance -> property of an object is acquired by another object; IS-A relationship / parent-child relationship
+    ______________________________________________________________________________
+
+    Extension ---> Inheritance
+
+    Ex, for an eCommerce Solution
+
+    Product
+        pid
+        name
+        price
+
+   LEDTV extends Product
+        brand
+        technology
+
+   Mobile extends Product
+        ram
+        os
+        sdcard
+
+
+    Object is a real world entity with properties and behaviors
+    Class is a blueprint for object creation
+
+    OOP -> Bottom Up approach
+    -> Has access modifiers {for security}
+    -> Objects can move & communicate with each other through member functions
+    -> More secure
+    -> Supports overloading
+
+    Procedural -> Top-Down approach
+    -> Divided into functions
+    -> Doesn't have access modifiers
+    -> Data can move freely from function to function in the system
+    -> Less secure
+    -> Doesn't support overloading
+
+
+
+
+    */
+
+}
