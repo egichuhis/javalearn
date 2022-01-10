8e4cd67c07034e2b862684f8e27eedcaa570c297
Edwin Gichuhi <edwingichuhis@gmail.com>
2022-01-10 12:35:00 +0300

    java data structures

--- src/com/pentacom/FunctionalProgramming/Notes.java
+++ src/com/pentacom/FunctionalProgramming/Notes.java
@@ -2,12 +2,9 @@ package com.pentacom.FunctionalProgramming;
 
 public class Notes {
 
-/**
-Functional programming with Java
+//Functional programming with Java
+// Imperative vs Declarative programming
+// Java Functional Interfaces
 
 
-
-
-
- **/
 }
